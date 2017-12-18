ef1b3a28e9f01755ad8627005b5119ca72399371
Eric <zixia@umail.ucsb.edu>
2017-12-18 14:27:17 -0800

    update blog data

--- blog_data/proc.py
+++ blog_data/proc.py
@@ -2,13 +2,18 @@ import numpy as np
 from scipy import sparse
 import random
 import scipy.sparse
+import cPickle
+
+def save(obj, filename):
+    cPickle.dump(obj, open(filename, 'wb'))
 
 def save_sparse_csr(filename, array):
     np.savez(filename,data = array.data ,indices=array.indices,
              indptr =array.indptr, shape=array.shape )
 
 def blogx():
-    data = np.zeros((10312, 10312))
+    # sparse.coo_matrix((data, (row, col)), shape = (len(ents), f_num), dtype = np.float32).tocsr()
+    data = np.zeros((10312, 10312),dtype = np.float32)
     m,n = data.shape
     for i in range(m):
         data[i][i] = 1
@@ -25,14 +30,16 @@ def blogx():
     x = data[:int(10312*0.1)]
     vx = data[int(10312*0.1):int(10312*0.2)]
     tx = data[int(10312*0.2):]
-    xData = sparse.csr_matrix(x)
-    txData =sparse.csr_matrix(tx)
+    xData = sparse.csr_matrix(x, dtype = 'float32')
+    txData =sparse.csr_matrix(tx, dtype = 'float32')
     print xData, xData.shape
     print txData, txData.shape
     # scipy.sparse.save_npz('trans.blog.x', xData)
     # scipy.sparse.save_npz('trans.blog.tx', txData)
-    np.save('trans.blog.x', xData)
-    np.save('trans.blog.tx', txData)
+    save(xData, '../data/trans.blog.x')
+    save(txData, '../data/trans.blog.tx')
+    # np.save('trans.blog.x', xData)
+    # np.save('trans.blog.tx', txData)
     # save_sparse_csr('trans.blog.x', xData)
     # save_sparse_csr('trans.blog.tx', txData)
 
@@ -43,15 +50,19 @@ def blogy():
     yblog = blog[:int(10312*0.1)]
     vy = blog[int(10312*0.1):int(10312*0.2)]
     tyBlog = blog[int(10312*0.2):]
-    yblog = sparse.csr_matrix(yblog)
-    tyBlog =sparse.csr_matrix(tyBlog)
+    # yblog = sparse.csr_matrix(yblog)
+    # tyBlog =sparse.csr_matrix(tyBlog)
     # scipy.sparse.save_npz('trans.blog.y',yblog)
     # scipy.sparse.save_npz('trans.blog.ty',tyBlog)
-    np.save('trans.blog.y',yblog)
-    np.save('trans.blog.ty',tyBlog)
+
+    save(yblog.astype(np.int32),'../data/trans.blog.y')
+    save(tyBlog.astype(np.int32), '../data/trans.blog.ty')
+    # np.save('trans.blog.y',yblog)
+    # np.save('trans.blog.ty',tyBlog)
 
 
     print blog,'hahahahah', blog.shape
+    print yblog.astype(np.int32).dtype
 
 def graph():
     graph = {}
@@ -63,9 +74,12 @@ def graph():
                 graph[int(ls[0])].append(int(ls[1]))
             else:
                 graph[int(ls[0])] = []
+                graph[int(ls[0])].append(int(ls[1]))
         print graph
-    np.save('trans.blog.graph',graph)
+        save(graph, '../data/trans.blog.graph')
+    # np.save('trans.blog.graph',graph)
 
-blogx()
+
+# blogx()
 blogy()
-graph()
+# graph()
